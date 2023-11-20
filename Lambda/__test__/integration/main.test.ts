2f5e72b7eb7db3cce04c913e08adcfdf52394c75
Toby Loki <tobyloki5@gmail.com>
2023-11-20 13:22:30 -0800

    (1) added integration test with localstack (2) updated integration-test workflow to automate integratino test (3) updated instruction on README.md (4) Lambda Layer esbuild excludes build of packages

---
+++ Lambda/__test__/integration/main.test.ts
@@ -0,0 +1,55 @@
+import { describe, test, expect, beforeAll } from "bun:test";
+
+describe("Integration tests", () => {
+    let color: string;
+    beforeAll(() => {
+        const colors = [
+            "red",
+            "green",
+            "blue",
+            "purple",
+            "yellow",
+            "orange",
+            "black",
+            "white",
+        ];
+        const randomIndex = Math.floor(Math.random() * colors.length);
+        color = colors[randomIndex];
+    });
+
+    test("POST /color", async () => {
+        const res: Response = await fetch(
+            `${process.env.FUNCTION_URL}color?id=myId`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify({ color }),
+            }
+        );
+
+        const ok = res.ok;
+        const statusCode = res.status; // localstack returns 200 instead of 202
+        const body = await res.json();
+
+        expect(ok).toEqual(true);
+        expect(body).toHaveProperty("message");
+    });
+
+    test("GET /color", async () => {
+        // Delay the test execution for 1 second to give time for SQS to trigger Worker Lambda
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        const res: Response = await fetch(
+            `${process.env.FUNCTION_URL}color?id=myId`
+        );
+
+        const statusCode = res.status;
+        const body = await res.json();
+
+        expect(statusCode).toEqual(200);
+        expect(body).toHaveProperty("color");
+        expect(body.color).toEqual(color);
+    });
+});
