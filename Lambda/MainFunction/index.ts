bb6b67fa016a435ff99823adae5ba85b4961d905
tobyloki <tobyloki5@gmail.com>
2023-11-21 03:03:02 -0800

    update

--- Lambda/MainFunction/index.ts
+++ Lambda/MainFunction/index.ts
@@ -1,57 +1,60 @@
-import { Handler } from 'aws-lambda';
+import { Handler } from "aws-lambda";
 
-import { rootPath } from 'get-root-path';
-const layerPath = '/opt/nodejs/dist';
+import { rootPath } from "get-root-path";
+const layerPath = "/opt/nodejs/dist";
 
 const awsHelper =
-	process.env.NODE_ENV === 'test'
-		? await import(`${rootPath}/Lambda/Layer/nodejs/awsHelper.ts`)
-		: await import(`${layerPath}/awsHelper.mjs`);
+    process.env.NODE_ENV === "test"
+        ? await import(`${rootPath}/Lambda/Layer/nodejs/awsHelper.ts`)
+        : await import(`${layerPath}/awsHelper.mjs`);
 
 export const handler: Handler = async (event) => {
-	console.log(JSON.stringify(event, null, 2));
+    console.log(JSON.stringify(event, null, 2));
 
-	const request = {
-		path: event.requestContext.http.path,
-		method: event.requestContext.http.method,
-		query: event.queryStringParameters,
-		body: event.body && JSON.parse(event.body)
-	};
-	const { path, method, query, body } = request;
+    const request = {
+        path: event.requestContext.http.path,
+        method: event.requestContext.http.method,
+        query: event.queryStringParameters,
+        body: event.body && JSON.parse(event.body)
+    };
+    const { path, method, query, body } = request;
 
-	if (path == '/color') {
-		if (method == 'GET') {
-			const { id } = query;
+    if (path == "/color") {
+        if (method == "GET") {
+            const { id } = query;
 
-			const res = await awsHelper.getItem('devtestprodaws-database', id);
+            const res = await awsHelper.getItem("devtestprodaws-database", id);
 
-			let color = null;
-			if (res) {
-				color = res.color;
-			}
+            let color = null;
+            if (res) {
+                color = res.color;
+            }
 
-			return {
-				statusCode: 200,
-				body: JSON.stringify({ color })
-			};
-		} else if (method == 'POST') {
-			const { id } = query;
-			const { color } = body;
+            return Promise.resolve({
+                statusCode: 200,
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ color })
+            });
+        } else if (method == "POST") {
+            const { id } = query;
+            const { color } = body;
 
-			await awsHelper.sendMessage(
-				'devtestprodaws-queue',
-				JSON.stringify({ id, color })
-			);
+            await awsHelper.sendMessage(
+                "devtestprodaws-queue",
+                JSON.stringify({ id, color })
+            );
 
-			return {
-				statusCode: 202,
-				body: JSON.stringify({ message: 'Request is being processed' })
-			};
-		}
-	}
+            return Promise.resolve({
+                statusCode: 202,
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ message: "Request is being processed" })
+            });
+        }
+    }
 
-	return {
-		statusCode: 400,
-		body: JSON.stringify({ error: 'Unknown command' })
-	};
+    return Promise.resolve({
+        statusCode: 400,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ error: "Unknown command" })
+    });
 };
